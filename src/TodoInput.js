01ccbcbcc34533a199106211e4922102d8d8e395
eunwoo park <eeunwoop@gmail.com>
2020-08-01 17:23:25 +0900

    css jobs + input things

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -4,32 +4,50 @@ export class TodoInput {
         this.todoInput = document.getElementById('todo-input');
         this.enterButton = document.getElementById('enter-button');
 
-        // Get the modal
-        var modal = document.getElementById("myModal");
+        const modal = document.getElementById("input-window");
+        const btn = document.getElementById("myBtn");
+        const span = document.getElementsByClassName("close")[0];
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
+        const dateButton = document.getElementById('date-button');
+        const yoilButton = document.getElementById('yoil-button');
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+        const yoilChecks = document.getElementById('date-checks');
+        const dateChecks = document.getElementById('calendar');
 
-        // When the user clicks on the button, open the modal
+    
         btn.onclick = function () {
             modal.style.display = "block";
         }
-
-        // When the user clicks on <span> (x), close the modal
         span.onclick = function () {
             modal.style.display = "none";
         }
-
-        // When the user clicks anywhere outside of the modal, close it
         window.onclick = function (event) {
             if (event.target == modal) {
                 modal.style.display = "none";
             }
         }
         this.addEnterListener();
+
+        dateButton.onclick = () => {
+            if (dateChecks.style.visibility === 'visible') {
+                dateChecks.style.visibility = 'collapse';
+            } else {
+                dateChecks.style.visibility = 'visible';
+                if (yoilChecks.style.visibility === 'visible') {
+                    yoilChecks.style.visibility = 'collapse';
+                }
+            }
+        }
+        yoilButton.onclick = () => {
+            if (yoilChecks.style.visibility === 'visible') {
+                yoilChecks.style.visibility = 'collapse';
+            } else {
+                yoilChecks.style.visibility = 'visible';
+                if (dateChecks.style.visibility === 'visible') {
+                    dateChecks.style.visibility = 'collapse';
+                }
+            }
+        }
     }
 
     validateInput(value) {
