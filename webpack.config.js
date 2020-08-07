bb7478b58d550e1547b87abbcf31ba8312d26a42
eunwoo park <eeunwoop@gmail.com>
2020-08-08 01:51:55 +0900

    always development, tobe fixed well

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+	mode: 'development',
 	entry: {
 		index: path.join(__dirname, 'src', 'index.js'),
 		login: path.join(__dirname, 'src', 'Login.js')
